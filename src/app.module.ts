666d185f6e4906c9d24e3756b019e7b46cec8e2f
Milos Vuksanovic <hex3solutions@gmail.com>
2022-02-10 12:10:17 +0100

    Set typerom instance in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeormConfig from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeormConfig),
+  ],
+  
   controllers: [AppController],
   providers: [AppService],
 })
