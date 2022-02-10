886793c3c6d6e049324dbffff061786a5a96f2f9
Milos Vuksanovic <hex3solutions@gmail.com>
2022-02-10 14:03:55 +0100

    Create location entity

---
+++ src/entities/location/entity/location.entity.ts
@@ -0,0 +1,24 @@
+import { Car } from "src/entities/car/entity/car.entity";
+import { BaseEntity, Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Location extends BaseEntity{
+    @PrimaryGeneratedColumn('uuid')
+    id: number
+
+    @Column({nullable: false})
+    street: string
+
+    @Column({nullable: false})
+    city: string
+
+    @CreateDateColumn({ type: 'timestamp' })
+    created_at: Date;
+  
+    @UpdateDateColumn({ type: 'timestamp' })
+    updated_at: Date;
+
+    @OneToMany(() => Car, (car) => car.location_id, { onDelete: 'CASCADE' })
+    cars: Car[]
+
+}
