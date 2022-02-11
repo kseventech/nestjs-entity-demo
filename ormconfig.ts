9107df40442834822854abb03414215a4f03caec
Milos Vuksanovic <hex3solutions@gmail.com>
2022-02-11 10:45:12 +0100

    Remove or statment in db connection

--- ormconfig.ts
+++ ormconfig.ts
@@ -6,11 +6,11 @@ dotenv.config()
 
 const typeormConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'entity_demo',
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
   port: 5432,
-  username: process.env.DB_USERNAME || 'user_1',
-  password: process.env.DB_PASSOWRD || 'test1',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSOWRD,
   entities: ['dist/src/**/*.entity.js'],
   synchronize: false,
   migrations: ['dist/src/db/migrations/*.js'],
