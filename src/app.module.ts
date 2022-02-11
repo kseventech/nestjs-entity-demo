9274eb6a98151e026a75675995bc2e27cc782a91
Milos Vuksanovic <hex3solutions@gmail.com>
2022-02-11 12:39:03 +0100

    Add repository to module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import typeormConfig from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Car } from './entities/car/entity/car.entity';
+import { Color } from './entities/color/entity/color.entity';
+import { Location } from './entities/location/entity/location.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeormConfig),
+    TypeOrmModule.forFeature([Car,Location,Color])
   ],
   
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Car } from './entities/car/entity/car.entity';
+import { Color } from './entities/color/entity/color.entity';
+import { Location } from './entities/location/entity/location.entity';
 
 @Injectable()
 export class AppService {
+  
+
+  constructor(
+    @InjectRepository(Car) private carRepo: Repository<Car>,
+    @InjectRepository(Color) private colorRepo: Repository<Color>,
+    @InjectRepository(Location) private locationRepo: Repository<Location>
+  ) {}
   getHello(): string {
     return 'Hello World!';
   }
