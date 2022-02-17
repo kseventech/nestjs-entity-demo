af002c41ca59fec751ec93224ca2e619a92ab2ac
Milos Vuksanovic <hex3solutions@gmail.com>
2022-02-17 10:11:34 +0100

    Insert entities module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import typeormConfig from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CarModule } from './entities/car/car.module';
 import { Car } from './entities/car/entity/car.entity';
+import { ColorModule } from './entities/color/color.module';
 import { Color } from './entities/color/entity/color.entity';
 import { Location } from './entities/location/entity/location.entity';
+import { LocationModule } from './entities/location/location.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeormConfig),
-    TypeOrmModule.forFeature([Car,Location,Color])
+    TypeOrmModule.forFeature([Car,Location,Color]),
+    CarModule,
+    LocationModule,
+    ColorModule
   ],
   
   controllers: [AppController],
