19673c41bb25f00f0065f613db16665bdee1fc6e
Milos Vuksanovic <hex3solutions@gmail.com>
2022-02-17 13:04:34 +0100

    Add validation pipes to global nest app

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { uuidValidationPipe, validationPipe } from './common/pipes';
 import { swaggerConfig, swaggerOptions } from './config/swagger.config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document, swaggerOptions);
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, swaggerConfig), swaggerOptions);
+  app.useGlobalPipes(validationPipe, uuidValidationPipe);
   await app.listen(3000);
 }
 bootstrap();
