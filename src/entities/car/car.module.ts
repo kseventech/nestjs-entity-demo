048bbd815d03acb5084fc4d1c24401e9fa0ab8ff
Milos Vuksanovic <hex3solutions@gmail.com>
2022-02-18 09:43:29 +0100

    Remove module imports in car

--- src/entities/car/car.module.ts
+++ src/entities/car/car.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { LocationModule } from "../location/location.module";
 import { CarController } from "./car.controller";
 import { CarService } from "./car.service";
 import { Car } from "./entity/car.entity";
